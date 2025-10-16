3145d24124dc07c7a31827bb5f5eb0655adb6479
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-10-16 13:51:40 +0700

    fix: firebase setup issue on flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:pentagram/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
